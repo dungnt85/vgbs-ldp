84454f878768fe0718f76a90ffdc687f2eec1ac8
dungnt85 <dungnt85@hotmail.com>
2024-08-20 18:17:55 +0700

    Update navigation.jsx

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            QVGBS™ 2024
+            VGBS™ 2024
           </a>{" "}
         </div>
 
@@ -39,7 +39,7 @@ export const Navigation = (props) => {
             </li>
             <li>
               <a href="#team" className="page-scroll">
-                Diễn giả Q
+                Diễn giả
               </a>
             </li>
             <li>
