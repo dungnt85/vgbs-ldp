cf36cb7e7233997d0ed66967aef2334e9fa99279
dungnt85 <dungnt85@hotmail.com>
2024-08-22 10:44:02 +0700

    Update navigation.jsx

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -43,7 +43,7 @@ export const Navigation = (props) => {
               </a>
             </li>
             <li>
-              <a href="#sponsor" className="page-scroll">
+              <a href="#sponsors" className="page-scroll">
                 Đối tác
               </a>
             </li>
