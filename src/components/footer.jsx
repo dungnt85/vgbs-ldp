ae433e9135f2c5d5dd61e9ce7351b6545d85ba0e
dungnt85 <dungnt85@hotmail.com>
2024-08-24 22:15:52 +0700

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,12 +8,12 @@ export const Footer = (props) => {
         <div className="container">
           <div className="col-md-8">
             <div className="row">
-              <div className="col-md-2">
+              <div className="col-md-2 col-sm-4 col-4">
                 <div className="logo-image">
                   <img src="/img/vbp-footer.png" alt="" />
                 </div>
               </div>
-              <div className="col-md-2">
+              <div className="col-md-2 col-sm-4 col-4">
                 <div className="logo-image">
                   <img src="/img/vbp-footer.png" alt="" />
                 </div>
