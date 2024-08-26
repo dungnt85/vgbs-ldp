cc316245905be80dc1e7d6f9ff90bfdcb665d4af
dungnt85 <dungnt85@hotmail.com>
2024-08-27 00:14:14 +0700

    updates

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,7 +5,7 @@ export const About = (props) => {
     <div id="about">
       <div className="container">
         <div className="row">
-          <div className="col-xs-8 col-md-6 about-bg">
+          <div className="col-xs-12 col-md-6 about-bg">
             <h2>Vietnam Global Brand Summit™ 2024: Brand-Led Sustainability™</h2>
             <h3>From social responsibility to Transforming sustainable consumption habits</h3>
           </div>
