9ad7ef553b05094e95d3705c1b2fe4e6a5f387e0
dungnt85 <dungnt85@hotmail.com>
2024-08-26 23:43:20 +0700

    UPDATES

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,27 +11,7 @@ export const About = (props) => {
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
-              <p>{props.data ? props.data.paragraph : "loading..."}</p>
-              <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`}> {d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-              </div>
+              <h3>{props.data ? props.data.paragraph : "loading..."}</h3>
             </div>
           </div>
         </div>
