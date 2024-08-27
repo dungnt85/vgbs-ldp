308c7f63bd79e2c397c5b52a7f5e47e52f0be9e4
dungnt85 <dungnt85@hotmail.com>
2024-08-27 13:22:00 +0700

    u

--- src/components/partners.jsx
+++ src/components/partners.jsx
@@ -8,7 +8,13 @@ export const Partners = (props) => {
           <h2>Strategic Partners</h2>
         </div>
         <div className="col-xs-12 col-md-12 row center sponsors-image">
-          <img src="img/partners.png" alt="" />
+          <img src="img/partners/strategic.png" alt="" />
+        </div>
+        <div className="section-title text-center">
+          <h2>Media Partners</h2>
+        </div>
+        <div className="col-xs-12 col-md-12 row center sponsors-image">
+          <img src="img/partners/media.png" alt="" />
         </div>
         <div className="row center">
           {/*<a href="mailto:anh.lq@purposeant.asia?subject=Partnership of Vietnam Global Brand Summit (TM)" className="btn btn-custom btn-lg page-scroll">
