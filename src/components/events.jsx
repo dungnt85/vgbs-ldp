63cde02023fb95f3e3f57eaad6ad173074fddb43
dungnt85 <dungnt85@hotmail.com>
2024-08-27 12:20:12 +0700

    u

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -10,7 +10,7 @@ export const Events = (props) => {
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className="col-xs-12 col-sm-10 col-md-5 col-lg-5">
+                <div key={`${d.title}-${i}`} className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
                   {" "}
                   <i className={d.icon}></i>
                   <h3>{d.title}</h3>
