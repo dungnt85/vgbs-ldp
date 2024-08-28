e0f0d8e0d3405fe05a761ae59fdd8dd674146154
dungnt85 <dungnt85@hotmail.com>
2024-08-28 08:20:25 +0700

    updates

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -7,16 +7,14 @@ export const Events = (props) => {
         <div className="col-md-10 col-md-offset-1 section-title">
           <h2>Key Events</h2>
         </div>
-        <div className="row">
+        <div className="col-md-10 col-md-offset-1 row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <div key={`${d.title}-${i}`} className="col-xs-12 col-sm-12 col-md-6 col-lg-6 event-item">
                   {" "}
                   <img src={d.img} alt="" className="event-img" />
-                  <div className="event-item">
-                    <h3>{d.subtitle}</h3>
-                    <p>{d.text}</p>
-                  </div>
+                  <h3>{d.subtitle}</h3>
+                  <p>{d.text}</p>
                 </div>
               ))
             : "Loading..."}
