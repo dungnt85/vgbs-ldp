46aa481dff87289e27c7c4d82417ceedfc65171a
dungnt85 <dungnt85@hotmail.com>
2024-08-28 10:20:58 +0700

    update events responsive

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -10,7 +10,7 @@ export const Events = (props) => {
         <div className="col-md-10 col-md-offset-1 row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className="col-xs-10 col-sm-10 col-md-5 event-item">
+                <div key={`${d.title}-${i}`} className="col-xs-10 col-sm-10 col-md-4 col-md-offset-1 event-item">
                   {" "}
                   <img src={d.img} alt="" className="event-img" />
                   <h3>{d.subtitle}</h3>
